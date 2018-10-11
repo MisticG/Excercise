b876a277351d7daa00d6beafa019dd123912b937
Your Name <you@example.com>
2018-10-11 10:52:28 +0200

    added list of names and printed out on html

---
+++ script.js
@@ -0,0 +1,14 @@
+
+var list = [];
+
+var people = [
+    'mamma ',
+    'pappa ',
+    'michel ',
+    'amy ',
+    'Henrik ',
+    'Emi ',
+    'Hugo '
+];
+
+document.getElementById("main").innerHTML = people;
